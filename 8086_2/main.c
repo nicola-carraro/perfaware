3f1a7b85b9f19ba48eff72d2d977e8e3d68cf5c2
Nicola Carraro <carraron985@gmail.com>
2023-04-02 10:09:39 +0200

    Created some structs

--- 8086_2/main.c
+++ 8086_2/main.c
@@ -3,6 +3,76 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdarg.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+#define REG_COUNT 8
+
+typedef enum
+{
+    a,
+    b,
+    c,
+    d,
+    sp,
+    bp,
+    si,
+    di
+} Register;
+
+typedef enum
+{
+    x,
+    l,
+    h
+} RegisterPortion;
+
+typedef struct
+{
+    Register reg;
+    RegisterPortion portion;
+} RegisterLocation;
+
+typedef struct
+{
+    RegisterLocation reg0;
+    RegisterLocation reg1;
+    uint8_t regCount;
+    int16_t displacement;
+} MemoryLocation;
+
+typedef enum
+{
+    memoryOperand,
+    registerOperand,
+    immediateOperand
+} OperandType;
+
+typedef struct
+{
+    OperandType type;
+    union location
+    {
+        MemoryLocation memory;
+        RegisterLocation reg;
+    };
+} Operand;
+
+const struct
+{
+    const Register reg;
+    const char name[3];
+    const bool isPartiallyAdressable;
+} RegisterInfos[] = {
+    {a, "a", true},
+    {b, "b", true},
+    {c, "c", true},
+    {d, "d", true},
+    {sp, "sp", false},
+    {bp, "bp", false},
+    {si, "si", false},
+    {di, "di", false}
+};
 
 void printPressEnterToContinue()
 {
@@ -15,20 +85,25 @@ void printPressEnterToContinue()
     }
 }
 
-void error(char *format, ...)
+void printError(const char *format, va_list args)
 {
     char errorMessage[256];
-
-    va_list args;
-    va_start(args, format);
     vsprintf(errorMessage, format, args);
     va_end(args);
     perror(errorMessage);
+}
+
+void error(const char *format, ...)
+{
+
+    va_list args;
+    va_start(args, format);
+    printError(format, args);
 
     exit(EXIT_FAILURE);
 }
 
-void readFile(char *filename, char **bytes, size_t *len)
+char *readFile(const char *filename, size_t *len)
 {
     FILE *file = fopen(filename, "rb");
     if (file == NULL)
@@ -48,7 +123,7 @@ void readFile(char *filename, char **bytes, size_t *len)
         error(errorMessage, filename);
     }
 
-    *bytes = malloc(*len);
+    char *bytes = malloc(*len);
 
     if (!bytes)
     {
@@ -59,16 +134,17 @@ void readFile(char *filename, char **bytes, size_t *len)
     {
         error(" Could not read from % s ", filename);
     }
+
+    return bytes;
 }
 
 int main(void)
 {
-    char filename[] = "data/listing37";
+    char filename[] = "data/listing37f";
 
-    char *bytes;
     size_t len;
 
-    readFile(filename, &bytes, &len);
+    char *bytes = readFile(filename, &len);
 
     if (bytes != NULL)
     {
