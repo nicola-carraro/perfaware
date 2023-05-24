f0a2991be2fd42fa2a4fcd23ca1ce6ab336313e7
Nicola Carraro <carraron985@gmail.com>
2023-05-24 22:05:02 +0200

    Generate pairs in clusters

--- haversine/input.c
+++ haversine/input.c
@@ -6,8 +6,18 @@
 #include "stdlib.h"
 #include "assert.h"
 #include "stdarg.h"
+#include "string.h"
 
 #define OUTPUT_PATH "data/input.json"
+#define CLUSTER_COUNT 16
+
+typedef struct
+{
+    double minX;
+    double maxX;
+    double maxY;
+    double minY;
+} Cluster;
 
 bool isDigits(char *cString)
 {
@@ -24,13 +34,13 @@ bool isDigits(char *cString)
     return true;
 }
 
-float randomFloat(float min, float max)
+double randomDouble(double min, double max)
 {
     assert(min <= max);
-    float range = max - min;
+    double range = max - min;
     int randomInt = rand();
 
-    float result = min + ((range / (float)RAND_MAX) * randomInt);
+    double result = min + ((range / (double)RAND_MAX) * randomInt);
 
     return result;
 }
@@ -59,29 +69,94 @@ void writeToFile(FILE *file, const char *fileName, const char *format, ...)
     }
 }
 
+double randomLatitude()
+{
+    return randomDouble(-90.0, 90.0);
+}
+
+double randomLongitude()
+{
+    return randomDouble(0.0, 180.0);
+}
+
+Cluster initializeCluster()
+{
+    Cluster cluster = {0};
+
+    double x1 = randomLongitude();
+    double x2 = randomLongitude();
+    double y1 = randomLatitude();
+    double y2 = randomLatitude();
+
+    double minx = min(x1, x2);
+    double maxx = max(x1, x2);
+    double miny = min(y1, y2);
+    double maxy = max(y1, y2);
+
+    cluster.minX = minx;
+    cluster.maxX = maxx;
+    cluster.minY = miny;
+    cluster.maxY = maxy;
+
+    return cluster;
+}
+
+bool cStringsEqual(char *left, char *right)
+{
+    return strcmp(left, right) == 0;
+}
+
+double square(double n)
+{
+    return n * n;
+}
+
+double degreesToRadians(double degrees)
+{
+    return degrees * 0.01745329251994329577;
+}
+
+// double haversine(double x1, double y1, double x2, double y2){
+
+//     double deltaLatitude = degreesToRadians(x2 - x1);
+//     double deltaLongitude = degreesToRadians(y2 - y1);
+// }
+
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
+    if (argc < 4)
     {
-        printf("Usage: %s pairs seed", argv[0]);
+        printf("Usage: %s method pairs seed", argv[0]);
         return;
     }
 
-    if (!isDigits(argv[1]))
+    bool clustered = false;
+
+    if (cStringsEqual(argv[1], "cluster"))
     {
-        printf("Error: pairs should be numeric, found: %s", argv[1]);
+        clustered = true;
+    }
+    else if (!cStringsEqual(argv[1], "random"))
+    {
+        printf("Invalid method (allowed : cluster, random): %s", argv[0]);
         return;
     }
 
     if (!isDigits(argv[2]))
     {
-        printf("Error: seed should be numeric, found: %s", argv[2]);
+        printf("Error: pairs should be numeric, found: %s", argv[2]);
         return;
     }
 
-    size_t pairs = (size_t)atoi(argv[1]);
+    if (!isDigits(argv[3]))
+    {
+        printf("Error: seed should be numeric, found: %s", argv[3]);
+        return;
+    }
 
-    int seed = atoi(argv[2]);
+    size_t pairs = (size_t)atoi(argv[2]);
+
+    int seed = atoi(argv[3]);
 
     printf("Pairs : %zu\n", pairs);
     printf("Seed  : %d\n", seed);
@@ -98,13 +173,39 @@ int main(int argc, char *argv[])
 
     writeToFile(file, OUTPUT_PATH, "{\n\t\"pairs\":[\n");
 
+    Cluster clusters[CLUSTER_COUNT];
+
+    if (clustered)
+    {
+        for (size_t clusterIndex = 0; clusterIndex < CLUSTER_COUNT; clusterIndex++)
+        {
+            clusters[clusterIndex] = initializeCluster();
+        }
+    }
+
     for (size_t pair = 0; pair < pairs; pair++)
     {
-        float x1 = randomFloat(0.0f, 180.0f);
-        float y1 = randomFloat(-90.0f, 90.0f);
 
-        float x2 = randomFloat(0.0f, 180.0f);
-        float y2 = randomFloat(-90.0f, 90.0f);
+        double x1;
+        double y1;
+        double x2;
+        double y2;
+
+        if (clustered)
+        {
+            Cluster cluster = clusters[pair % CLUSTER_COUNT];
+            x1 = randomDouble(cluster.minX, cluster.maxX);
+            y1 = randomLatitude(cluster.minY, cluster.maxY);
+            x2 = randomLongitude(cluster.minX, cluster.maxX);
+            y2 = randomLatitude(cluster.minY, cluster.maxY);
+        }
+        else
+        {
+            x1 = randomLongitude();
+            y1 = randomLatitude();
+            x2 = randomLongitude();
+            y2 = randomLatitude();
+        }
 
         writeToFile(
             file,
