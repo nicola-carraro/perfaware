231a67bcb4b6b87e64ec83baacb03dcfeda2bbe2
Nicola Carraro <carraron985@gmail.com>
2023-05-28 23:11:58 +0200

    Improved testing of floats output

--- haversine/test.c
+++ haversine/test.c
@@ -7,12 +7,16 @@ int main(void)
 {
     FILE *file = fopen("data/pairs.double", "r");
 
-    for (size_t i = 0; i < 100; i++)
+    for (size_t i = 0; i < 10; i++)
     {
         for (size_t j = 0; j < 4; j++)
         {
             double d;
-            fread(&d, sizeof(double), 1, file);
+            if (fread(&d, sizeof(double), 1, file) != 1)
+            {
+                printf("Reached end");
+                return;
+            }
             printf("%f ", d);
         }
         printf("\n");
